e3c39bc88ac1962bbf1ac885cd643bf8126647eb
didierdelhaisse <did35@yahoo.com>
2019-08-30 09:39:34 +0200

    First commit

---
+++ TextRecognition/Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by Didier Delhaisse on 30/08/2019.
+//  Copyright © 2019 Didier Delhaisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func translateBtn(_ sender: Any) {
+        // To do
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        // To do
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
